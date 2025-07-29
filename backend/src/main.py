e0b5e0368c939d2528b7c9baee2a84280e796fbc
KanishkNoir <kanishksingh.1106@gmail.com>
2025-07-29 13:07:34 +0530

    path fix

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
-from src.api.db import init_db
-from src.api.chat.routing import router as chat_router
+from api.db import init_db
+from api.chat.routing import router as chat_router
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
